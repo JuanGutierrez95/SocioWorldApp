6d5c703cd4e064d6d6804e279249d26238d3c944
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-01 20:30:59 -0300

    configurando archivo index.js

--- server/index.js
+++ server/index.js
@@ -3,3 +3,5 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
