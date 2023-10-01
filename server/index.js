69900840d01d149bba3d0d58b57aae8a54b52f26
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-01 20:39:31 -0300

    configuraciones en el archivo index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-/* MIDDLEWARE */
+/* CONFIGURACIONES */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
