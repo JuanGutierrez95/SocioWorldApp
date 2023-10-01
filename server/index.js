1c4555d614854db81deec05ad8eb9287622f0217
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-01 20:40:47 -0300

    configuraciones en el archivo index.js

--- server/index.js
+++ server/index.js
@@ -14,3 +14,5 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(helmet());
