7e37aaa8c9c86c50c1a7181b0eb1235d6bd9667b
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-01 20:34:57 -0300

    configurando middleware

--- server/index.js
+++ server/index.js
@@ -9,3 +9,6 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+/* MIDDLEWARE */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
