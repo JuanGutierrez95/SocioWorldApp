5759289becad954b1c19cecf5cb47a6311fc08da
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-12 02:21:45 -0300

    trabajando en register user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -20,7 +20,18 @@ export const register = async (req, res) => {
     const passwordHash = await bcrypt.hash(password, salt);
 
     const newUser = new User({
-        
-    })
-} catch (error) {}
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {}
 };
