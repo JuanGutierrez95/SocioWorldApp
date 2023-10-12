a08545a6472fa29631d93d315caccdf7b058e90b
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-12 02:19:29 -0300

    trabajando en register user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,9 +4,23 @@ import User from "../models/User.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
-    try {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
         
-    } catch (error) {
-        
-    }
-}
+    })
+} catch (error) {}
+};
