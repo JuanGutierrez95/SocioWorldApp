dddd8663ba6b80a9b3e6c5e62745bf1761b0a773
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-12 02:10:35 -0300

    creando register user

---
+++ server/controllers/auth.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
